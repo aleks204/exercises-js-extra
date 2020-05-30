5d3c4290d0b13fdacf7a3f9d2ba53688c24fc6fa
Aleks Andros <1445232@gmail.com>
2020-05-30 08:53:34 +0200

    find a word by..

--- exercises/1-loops/2-find.js
+++ exercises/1-loops/2-find.js
@@ -1,30 +1,12 @@
-/*
-Given an array of cities create a function that returns the number of cities that start with 'A'
-
-It may be useful know that:
-- in strings you can use the function startsWith()
-- we can iterate arrays
-- we know the lenght of an array by using length
-
-
-var name = "Jose";
-console.log(name + " starts with E = " + name.startsWith('E'));
-console.log(name + " starts with J = " + name.startsWith('J'));
-
-const namesArray = ["Jose","Juan"];
-console.log(namesArray[0]); //"Jose"
-console.log(namesArray.length); //2
-*/
-
-function find(cities, letter){
-	var counter = 0;
-	//your code here
-	return counter;
+function find(cities, letter) {
+  var counter = [];
+  for (let i = 0; i < cities.lenght; i++) {
+    if (cities[i].startsWith(letter)) counter.push(cities[i]);
+  }
+  return counter.length;
 }
-
 
 const citiesArray = ["Amsterdam", "Athens", "Barcelona", "Madrid"];
 
-
-console.log("Number of cities with A = " + find(citiesArray, 'A'));
-console.log("Number of cities with B = " + find(citiesArray, 'B'));
+console.log("Number of cities with A = " + find(citiesArray, "A"));
+console.log("Number of cities with B = " + find(citiesArray, "B"));
